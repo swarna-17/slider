8a588aeac8c37a4c224213c537b523dd36178e78
SWARNA <75875736+swarna-17@users.noreply.github.com>
2022-10-19 15:52:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("list").style.width = "250px";
+    document.getElementById("content").style.marginLeft = "250px";
+  }
+  function closeNav() {
+    document.getElementById("list").style.width = "0";
+    document.getElementById("content").style.marginLeft= "0px";
+  }
